70b820224b730c323910c8e69fed7846bdf8c44d
neharaut00 <neharaut372@gmail.com>
2023-03-25 11:37:37 +0530

    html and static files added

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import tensorflow_hub as hub 
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.models import load_model
+from keras.utils import img_to_array
+from keras.utils import load_img
+from werkzeug.utils import secure_filename
+import os
+import sys
+
+class_map = ['Bean', 'Bitter Gourd', 'Bottle Gourd', 'Brinjal', 'Broccoli', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Cucumber', 'Papaya', 'Potato', 'Pumpkin', 'Radish', 'Tomato']
+
+# Preparing and pre-processing the image
+def load_and_prep_image(img_path):  
+    """
+    Reads an image from filename, turns it into a tensor
+    and reshapes it to (img_shape, img_shape, colour_channel).
+    """
+    # Read in target file (an image)
+    img = tf.io.read_file(img_path)
+
+    # Decode the read file into a tensor & ensure 3 colour channels 
+    # (our model is trained on images with 3 colour channels and sometimes images have 4 colour channels)
+    img = tf.image.decode_image(img, channels=3)
+
+    # Resize the image (to the same size our model was trained on)
+    img = tf.image.resize(img, size = [224, 224])
+
+    # Rescale the image (get all values between 0 and 1)
+    img = img/255.
+    # print(img)
+    return img
+
+def pred_and_plot(filename):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+  print("predict")
+
+  # Loading model
+  model = load_model("F:/Quark/ML workshop/ML workshop project/model_1.h5", custom_objects = {'KerasLayer':hub.KerasLayer})
+  
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  pred_class = class_map[int(tf.round(pred)[0][0])-1] 
+
+  return pred_class
+
